70f26da523d78c08f69e91d87e232e19c953f212
lnvariant <hamzayousaf0@hotmail.com>
2018-04-03 22:26:49 -0400

    New mongolab database

--- app/models/db-connection.js
+++ app/models/db-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost:3001/CryptoNut', (error) => {
+mongoose.connect('mongodb://clockfifty:CSC309@ds133659.mlab.com:33659/heroku_t1spdpl5', (error) => {
   if (error) console.log(error);
 
   console.log('Database connection successful');
